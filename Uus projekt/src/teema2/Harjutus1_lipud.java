b9dec6cef9ee62b930f81b248df5e244bbcae5f8
ltaimre <liisitaimre@gmail.com>
2015-10-31 16:09:34 +0200

    heh

--- Uus projekt/src/teema2/Harjutus1_lipud.java
+++ Uus projekt/src/teema2/Harjutus1_lipud.java
@@ -1,8 +1,15 @@
 package teema2;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * 1. Joonista eesti lipp kasutades kolme Rectangle
  * 2. Joonista Jaapani lipp kasutades Rectangle ja Circle
  */
-public class Harjutus1_lipud {
+public class Harjutus1_lipud extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
